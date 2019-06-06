e718c9a31e043fcd149b4a061cb384d3c2587460
Miguel Fernández <miguelrf95@gmail.com>
2019-06-06 16:17:36 +0100

    Level half-way

---
+++ GD_Prototipo2/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Instance variables
+    public float stillTime;
+
+    // Class variables
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stillTime -= Time.deltaTime;
+
+        if(stillTime <= 0)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
--- GD_Prototipo2/Assets/Scripts/ClearOnTouch.cs
+++ GD_Prototipo2/Assets/Scripts/ClearOnTouch.cs
@@ -2,25 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class ClearOnTouch : MonoBehaviour
 {
-    // Declare instance variables
-    private float speed = 2000.0f;
-
-    private Rigidbody2D rb;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        rb.velocity = transform.right * speed;
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Enemy")
