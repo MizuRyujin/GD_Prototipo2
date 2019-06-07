599da425355611f3b7133bf5de529b0ae76d7905
Miguel Romão Fernández <miguelrf95@gmail.com>
2019-06-07 11:35:38 +0100

    Jump is on point, enemy bullets cant go through wall

--- GD_Prototipo2/Assets/Scripts/WinCond.cs
+++ GD_Prototipo2/Assets/Scripts/WinCond.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WinCond : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        print("Player won!!!");
         if(other.tag == "Player")
         {
             Application.Quit();
